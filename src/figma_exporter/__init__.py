2c038cf48ab789ada53cc520844f30df3eb55afa
konard <drakonard@gmail.com>
2026-06-29 13:01:00 +0000

    Implement Figma file exporter

---
+++ src/figma_exporter/__init__.py
@@ -0,0 +1,6 @@
+"""Figma file exporter."""
+
+from figma_exporter.exporter import FigmaExporter
+
+__all__ = ["FigmaExporter"]
+__version__ = "0.1.0"
---
+++ src/figma_exporter/__main__.py
@@ -0,0 +1,3 @@
+from figma_exporter.cli import main
+
+raise SystemExit(main())
